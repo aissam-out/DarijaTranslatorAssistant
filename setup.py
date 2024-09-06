e4f4168bc1b7cb02094b9a7acc91e8c11e3cd9b5
aissam-out <aissam.outchakoucht@gmail.com>
2024-09-06 10:54:11 +0100

    change lib versions

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     name='DarijaTranslatorAssistant',
-    version='1.0.7',
-    description='A library for assisting in translating darija to english. It provides a list of potential translations for a given darija word. It also supports translation of full sentences using LLMs (e.g., OpenAI).',
+    version='1.0.0',
+    description='A library for assisting in translating Darija to English. It provides a list of potential translations for a given darija word. It also supports translation of full sentences using LLMs (e.g., OpenAI).',
     author='Aissam Outchakoucht',
     author_email='aissam.outchakoucht@gmail.com',
     url='https://github.com/aissam-out/DarijaTranslatorAssistant',
@@ -12,7 +12,7 @@ setup(
     packages=['DarijaTranslatorAssistant'],
     include_package_data=True,
     install_requires=[
-        'DarijaDistance==1.0.7',
+        'DarijaDistance==1.0.8',
         'openai==1.43.0',
         'requests==2.32.3',
     ],
